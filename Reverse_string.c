9b1c978475a902fc6eeda260751ae5b9ac6daa9c
Jegan PS <jeganpsnextmove@gmail.com>
2026-02-06 00:06:47 +0530

    Reversing a string reading

--- Reverse_string.c
+++ Reverse_string.c
@@ -1,36 +1,37 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
-char * rev_string(char *arr)
-{
-    int i,j;
-    char temp;
-    int len;
-    len=strlen(arr);
-    for(i=0,j=len-1;i<j;i++,j--)
-    {
-        temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-    return arr;
-}
-
 int main()
 {
-    char * arr;
-    int capacity;
-    printf("Enter the capacity of the string: ");
-    scanf("%d", &capacity);
-        getchar();  // Clear newline from scanf
-    arr=calloc(capacity+1, sizeof(char));
-   
-    fgets(arr, capacity+1, stdin);
-    rev_string(arr);
-    for(int i=0;arr[i]!='\0';i++)
+    char str[100];
+    printf("Enter the strings:\n");
+    fgets(str,sizeof(str),stdin);
+      int len=strlen(str);
+   if (str[len-1]== '\n') 
     {
-        printf("%c",arr[i]);
+        str[len-1] = '\0';
+        len--;
     }
-    free(arr);
+  
+    int end=len-1;
+    char sentence[100];
+    printf("Reversed words: ");  
+    for(int i=len-1;i>=0;i--)
+    {
+        if(i==0 || str[i]==' ')
+        {
+            int start;
+            if(i==0)
+                start=0;
+            else
+                start=i+1;
+            for(int j=start;j<=end;j++)
+            {        
+                       
+        printf("%c",str[j]);
+            }
+            printf(" ");
+            end=i-1;
+        }
+    }
     return 0;
 }
